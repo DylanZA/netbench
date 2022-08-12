c9df37d27c28316fceb967405b8c35ed540db4ba
Dylan Yudaken <dylany@fb.com>
2022-08-12 05:13:26 -0700

    minimise check.sh to run on most kernels
    Signed-off-by: Dylan Yudaken <dylany@fb.com>

--- check.sh
+++ check.sh
@@ -7,6 +7,4 @@ echo "using executable $TARGET"
 
 # don't test fixed_files as older kernels dont have it
 # old provide buffers implementations were a bit poor
-$TARGET --tx epoll --rx epoll --rx "io_uring --register_ring 0 --provide_buffers 0 --fixed_files 0" --time 1
-
-$TARGET --tx "io_uring  --threads 1 --per_thread 16" --rx "io_uring --register_ring 0 --provide_buffers 1 --fixed_files 0" --time 1
+$TARGET --v6 0 --tx epoll --rx epoll --rx "io_uring --register_ring 0 --provide_buffers 0 --fixed_files 0" --time 1
